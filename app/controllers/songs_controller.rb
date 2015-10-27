97bfe7445402cf3532e374635fc651be60021f6b
tuskerette <valeria.graffeo@gmail.com>
2015-10-26 23:23:32 -0400

    added methods new and create

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,4 +4,18 @@ class SongsController < ApplicationController
     @songs = Song.all
   end
 
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.create(songs_params)
+    # add if save...
+    redirect_to action: :index
+  end
+
+private
+  def songs_params
+    params.require(:song).permit(:url, :length)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'songs#index'
+  # resources :songs, only: :new
+  # resources :songs, except: [:new, :edit]
   resources :songs
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
