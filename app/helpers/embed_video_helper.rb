64bb7b98939b1ae9be70d77f7575f2f368917b34
tuskerette <valeria.graffeo@gmail.com>
2016-01-31 12:36:26 -0500

    yt turned to https

--- app/helpers/embed_video_helper.rb
+++ app/helpers/embed_video_helper.rb
@@ -3,7 +3,7 @@ module EmbedVideoHelper
   def embed(youtube_url, w, h)
     youtube_id = youtube_url.split("=").last
 
-    content_tag(:iframe, nil, src: "http://youtube.com/embed/#{youtube_id}", width:w, height:h)
+    content_tag(:iframe, nil, src: "https://youtube.com/embed/#{youtube_id}", width:w, height:h)
   end
 
 end
