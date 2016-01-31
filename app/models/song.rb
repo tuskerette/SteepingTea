b605f396c45ed830ab6831b536b4d50f1929cc63
tuskerette <valeria.graffeo@gmail.com>
2016-01-31 12:55:56 -0500

    edited time for white tea

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,8 +4,8 @@ class Song < ActiveRecord::Base
   validates :url, url: true
   before_create :set_duration
 
-  scope :white_tea, -> { where("duration > ? and duration < ?", 30, 60)}
-  scope :green_tea, -> { where("duration > ? and duration < ?", 60, 180)}
+  scope :white_tea, -> { where("duration > ? and duration < ?", 30, 90)}
+  scope :green_tea, -> { where("duration > ? and duration < ?", 90, 180)}
   scope :black_tea, -> { where("duration > ? and duration < ?", 180, 300) }
   scope :roiboos_tea, -> { where("duration > ? and duration < ?", 300, 420) }
   scope :herbal_tea, -> { where("duration > ?", 420) }
