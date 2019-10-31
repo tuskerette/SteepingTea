6c69ad59b74032d975476a04a3aa4c70d47b23cc
tuskerette <vgraffeo@everquote.com>
2019-10-31 15:01:42 -0400

    add auth code from google

--- config/initializers/yt.rb
+++ config/initializers/yt.rb
@@ -1,3 +1,4 @@
 Yt.configuration.api_key = ENV['YT_SECRET_TOKEN']
-
-
+Yt.configure do |config|
+  config.log_level = :debug
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   HighVoltage.configure do |config|
     config.home_page = 'home'
   end
-  
+
   resources :songs, except: [:show]
 end
