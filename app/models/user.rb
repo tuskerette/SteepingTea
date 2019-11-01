f447c49b5dc2ce0c434bf355258ad1138b461a5d
tuskerette <vgraffeo@everquote.com>
2019-11-01 11:50:46 -0400

    add dependent destroy to songs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :songs
+  has_many :songs, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
